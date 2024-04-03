656bb8fef3e5c65d00aaa250f58796f2e45c770c
Your Name <you@example.com>
2024-04-03 16:27:40 +0100

    Updated configure git to transfer origin

--- configure_git.sh
+++ configure_git.sh
@@ -1,6 +1,9 @@
 git config --global user.email "domduxbury@hotmail.com"
 git config --global user.name "Dominic Duxbury"
 
+git remote remove origin
+git remote add origin git@github.com:DomDuxbury/dotfiles-v2.git
+
 ssh-keygen -t ed25519 -C "domduxbury@hotmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
