3fab58373934819f4fe307bc0f2e87ed3f5311f4
Dominic Duxbury <domduxbury@hotmail.com>
2025-09-18 16:56:51 +0100

    ✨ Updated my dotfiles to work with AUR.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim zshrc oh-my-zsh tmux.conf tmux"    # list of files/folders to symlink in homedir
+files="vimrc vim zshrc gitconfig oh-my-zsh tmux.conf tmux"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -34,67 +34,14 @@ done
 mkdir -p $dir/tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Setup git config
+sudo pacman -S git-delta
+sudo pacman -S nvm
+npm install -g gitmoji
+gitmoji --config
+
 # Make vim backup swap and undo folders
 mkdir -p $dir/vim
 mkdir -p $dir/vim/_swap
 mkdir -p $dir/vim/_backup
 mkdir -p $dir/vim/_undo
-
-
-install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/robbyrussell/oh-my-zsh.git
-    fi
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        if [[ -f /etc/redhat-release ]]; then
-            sudo dnf install zsh
-            install_zsh
-        fi
-        if [[ -f /etc/debian_version ]]; then
-            sudo apt-get install zsh
-            install_zsh
-        fi
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
-    fi
-fi
-}
-
-install_pip () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /usr/bin/python3.11 ]; then
-    # Install Virtualenvwrapper
-    pip install virtualenvwrapper
-else
-    # Install python 3.11
-    sudo apt install build-essential checkinstall \ 
-    libreadline-gplv2-dev  libncursesw5-dev libssl-dev \
-    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev 
-    sudo add-apt-repository ppa:deadsnakes/ppa
-    sudo apt install python3.11 python3-pip
-
-    # Link python to python3.11
-    ln -s /usr/bin/python /usr/bin/python3.11
-
-    # Install pip
-    install_pip
-fi
-}
-
-sudo apt-get install thefuck
-install_zsh
-install_pip
-sudo apt-get install vim-gtk bat rip-grep jq
--- zshrc
+++ zshrc
@@ -21,20 +21,16 @@ alias vimf='vim $(fzf -m)'
 # Set Up Python Virtualenv Wrapper
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/code
-source ~/.local/bin/virtualenvwrapper.sh
+source /usr/bin/virtualenvwrapper.sh
 
 # Vim Alias
 alias vim="stty stop '' -ixoff ; vim"
-
-# Cat Alias
-alias cat=batcat
 
 ## Git Aliases
 alias gs='git status '
 alias ga='git add '
 alias gp='git push '
 alias gb='git branch '
-alias gc='git commit -m'
 alias gd='git diff'
 alias go='git checkout '
 alias gk='gitk --all&'
@@ -47,23 +43,36 @@ alias tm='tmux a'
 alias tms='tmux new-session -A -s'
 alias tmk='tmux kill-session -t'
 
+# Pacman Aliases
+alias browse='pacman -Ss '
+alias install='sudo pacman -S '
+
 ## Miscellaneous Aliases
-alias htop='sudo htop'
-alias imdone='git checkout master; deactivate'
-alias ipy='python -c "import IPython; IPython.terminal.ipapp.launch_new_instance()"'
-alias wine32='env WINEARCH=win32 WINEPREFIX="$HOME/.wine32" wine'
+alias ll='ls -a'
+alias top='btop'
+alias cat='bat'
 
 # Shell Functions
 # qfind - used to quickly find files that contain a string in a directory
-qfind () {
+qfind() {
   find . -exec grep -l -s $1 {} \;
   return 0
 }
 
-qGit () {
+gre () { 
+  rg $1 --json | delta
+  return 0
+}
+
+qGit() {
   git add -A
-  git commit -m $1
+  gitmoji -c --title="${1}."
   git push
+  return 0
+}
+
+gc() {
+  gitmoji -c --title="${1}."
   return 0
 }
 
@@ -71,20 +80,16 @@ rgv() {
   vim -q <(rg --vimgrep ${1})
 }
 
-qPrune () {
+qPrune() {
   git fetch -p && 
     for branch in `git branch -vv | grep ': gone]' | gawk '{print $1}'`; 
       do git branch -D $branch; 
     done
 }
 
-cleanDocker () {
+cleanDocker() {
   docker rm $(docker ps -q -a)
   docker rmi $(docker images | grep "none" | awk '/ / { print $3 }')
-}
-
-conv() {
-  sh /home/dom/Programs/sam2p-master/sam2p $1 $2
 }
 
 # The next line updates PATH for the Google Cloud SDK.
@@ -96,3 +101,4 @@ if [ -f '/home/dom/Programs/google-cloud-sdk/completion.zsh.inc' ]; then . '/hom
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source /usr/share/nvm/init-nvm.sh
